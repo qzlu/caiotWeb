5f164361e7af90904642a15d874a46ad4e95c7b9
qzlu <1014751531@qq.com>
2019-07-24 10:54:27 +0800

    更新

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -192,6 +192,7 @@ export default {
       var year = date.getFullYear();
       var month = date.getMonth() + 1;
       var strDate = date.getDate();
+      
       if (month >= 1 && month <= 9) {
         month = "0" + month;
       }
